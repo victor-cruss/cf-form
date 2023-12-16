1c56896c8876e09332bbb717191af7517bfd001e
victor-cruss <christosyonas@gmail.com>
2023-12-16 23:34:21 +0200

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,11 +2,9 @@
  * POST /api/submit
  */
 export async function onRequestPost({ request }) {
-	try {
-		let input = await request.formData();
-		// Create a prepared statement with our query
-	const ps = context.env.cf-demo-database.prepare('SELECT * from users');
+	// Create a prepared statement with our query
+	const ps = context.env.demo-cf-database.prepare('SELECT * from users');
 	const data = await ps.first();
   
 	return Response.json(data);
-}
+  }
