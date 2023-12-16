b28123368240b08bc1ebe9ca3c2de9a6c9d9a3a3
victor-cruss <christosyonas@gmail.com>
2023-12-16 23:44:48 +0200

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,9 +2,28 @@
  * POST /api/submit
  */
 export async function onRequestPost({ request }) {
-	// Create a prepared statement with our query
-	const ps = context.env.demo-cf-database.prepare('SELECT * from users');
-	const data = await ps.first();
-  
-	return Response.json(data);
-  }
+	try {
+		let input = await request.formData();
+
+		// Convert FormData to JSON
+		// NOTE: Allows mutliple values per key
+		let tmp, output = {};
+		for (let [key, value] of input) {
+			tmp = output[key];
+			if (tmp === undefined) {
+				output[key] = value;
+			} else {
+				output[key] = [].concat(tmp, value);
+			}
+		}
+
+		let pretty = JSON.stringify(output, null, 2);
+		return new Response(pretty, {
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			}
+		});
+	} catch (err) {
+		return new Response('Error parsing JSON content', { status: 400 });
+	}
+}
